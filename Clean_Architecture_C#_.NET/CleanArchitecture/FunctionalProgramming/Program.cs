814fc7aa4605831b0c7e823942d6f463f5547c3c
Antonio Narvaez <antonio.narvaez2002@gmail.com>
2025-07-06 20:47:36 -0600

    Principios SOLID

---
+++ Clean_Architecture_C#_.NET/CleanArchitecture/FunctionalProgramming/Program.cs
@@ -0,0 +1,103 @@
+﻿var t = TomorrowPure;
+Console.WriteLine(t(new DateTime(2024, 05, 01, 00, 00, 00)));
+
+//Action no retorna nada (void)
+Action<string> show = Console.WriteLine;
+show("Hola");
+
+Action<string> hi = name => Console.WriteLine($"Hola {name}"); 
+//Las expresiones lambda permite escribir funciones anonimas
+hi("Antonio");
+Action<int, int> add = (a, b) => show((a+b).ToString());
+add(5, 2);
+
+Func<int, int, int> mul = (a, b) => a * b; //El último valor de <> es el tipo que va a regresar
+// show(mul(3, 4).ToString());
+Func<int, int, string> mulString = (a, b) =>
+{
+    var res = a * b;
+    return res.ToString();
+};
+// show(mulString(5, 5));
+
+List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+Predicate<int> condition = x => x % 2 == 0; //Nos devuelve in booleano y no puede recibir mas de un parametro
+Predicate<int> condition2 = x => x > 5;
+
+var numbers2 = Filter(numbers, condition); 
+var numbers3 = Filter(numbers, condition2);
+
+foreach (var number in numbers2)
+{
+    Console.WriteLine(number);
+}
+
+foreach (var number in numbers3)
+{
+    Console.WriteLine(number);
+}
+
+
+List<int> Filter(List<int> list, Predicate<int> condition)
+{
+    var resultsList = new List<int>();
+    foreach (var element in list)
+    {
+        if (condition(element))
+        {
+            resultsList.Add(element);
+        }
+    }
+    return resultsList;
+}
+
+
+// var numbers2 = Filter(numbers, number => number % 2 == 0); 
+// //Podemos modificar su funcionamiento interno de una funcion de orden superior
+// var numbers3 = Filter(numbers, number => number > 5);
+
+// List<int> Filter(List<int> list, Func<int, bool> condition)
+// {
+//     var resultsList = new List<int>();
+//     foreach (var element in list)
+//     {
+//         if (condition(element))
+//         {
+//             resultsList.Add(element);
+//         }
+//     }
+//     return resultsList;
+// }
+
+//Funcion no pura
+DateTime Tomorrow()
+{
+    return DateTime.Now.AddDays(1);
+}
+
+Beer ToUpper(Beer beer)
+{
+    beer.Name = beer.Name.ToUpper();
+    return beer;
+}
+
+//Funcion Pura
+DateTime TomorrowPure(DateTime date)
+{
+    return date.AddDays(1);
+}
+
+Beer ToUpperPure(Beer beer)
+{
+    var beer2 = new Beer()
+    {
+        Name = beer.Name.ToUpper(),
+    };
+    return beer2;
+}
+
+public class Beer
+{
+    public string Name { get; set; }
+}
---
+++ Clean_Architecture_C#_.NET/CleanArchitecture/SOLID/Program.cs
@@ -0,0 +1,165 @@
+﻿var beerData = new BeerData();
+beerData.Add("Corona");
+beerData.Add("Delirium");
+beerData.Add("Erdinger");
+
+var reportGeneratorBeer = new ReportGeneratorBeer(beerData);
+var reportGeneratorHtmlBeer = new ReportGeneratorHtmlBeer(beerData);
+var report = new Report();
+var data = reportGeneratorBeer.Generate();
+//report.Save(reportGeneratorBeer, "cervezas.txt");
+report.Save(reportGeneratorHtmlBeer, "cervezas.html");
+
+
+public interface IReportGenerator
+{
+    public string Generate();
+}
+
+public class BeerData
+{
+    protected List<string> _beers;
+
+    public BeerData()
+    {
+        _beers = new List<string>();
+    }
+
+    public virtual void Add(string beer) => _beers.Add(beer);
+    public List<string> Get() => _beers;
+}
+
+// public class LimitedBeerData : BeerData
+// {
+//     private int _limit;
+//     
+//     public LimitedBeerData(int limit) => _limit = limit;
+//
+//     public override void Add(string beer)
+//     {
+//         if (_beers.Count >= _limit)
+//         {
+//             throw new InvalidOperationException("Limite de cervezas alcanzado");
+//         }
+//         base.Add(beer);    
+//     }
+// }
+
+public class LimitedBeerData
+{
+    private BeerData _beerData = new BeerData();
+    private int _limit;
+    private int _count;
+
+    public LimitedBeerData(int limit)
+    {
+        _limit = limit;
+    }
+
+    public void Add(string beer)
+    {
+        if (_count >= _limit)
+        {
+            throw new InvalidOperationException("Limite de cervezas alcanzado");
+        }
+        _beerData.Add(beer);
+        _count++;
+    }
+}
+
+public class ReportGeneratorBeer : IReportGenerator
+{
+    private BeerData _beerData;
+
+    public ReportGeneratorBeer(BeerData beerData)
+    {
+        _beerData = beerData;
+    }
+
+    public string Generate()
+    {
+        string data = "";
+        foreach (var beer in _beerData.Get())
+        {
+            data += "Cerveza: " + beer + Environment.NewLine; //Es una salgo de línea en un archivo de texto
+        }
+
+        return data;
+    }
+}
+
+public class ReportGeneratorHtmlBeer : IReportGenerator
+{
+    private BeerData _beerData;
+
+    public ReportGeneratorHtmlBeer(BeerData beerData)
+    {
+        _beerData = beerData;
+    }
+    public string Generate()
+    {
+        string data = "<div>";
+
+        foreach (var beer in _beerData.Get())
+        {
+            data += "<b>" + beer + "<b></br>";
+        }
+
+        data += "</div>";
+        return data;
+    }
+}
+
+public class Report
+{
+    public void Save(IReportGenerator reportGenerator, string filePath)
+    {
+        using (var writer = new StreamWriter(filePath))
+        {
+            string data = reportGenerator.Generate();
+            writer.WriteLine(data);
+        }
+    }
+}
+
+
+// Rectangle rectangle = new Square();
+// rectangle.setWidth(10);
+// rectangle.setHeight(20);
+// Console.WriteLine(rectangle.getArea());
+//
+// public class Rectangle
+// {
+//     private int _width;
+//     private int _height;
+//
+//     public virtual void setWidth(int width)
+//     {
+//         _width = width;
+//     }
+//
+//     public virtual void setHeight(int height)
+//     {
+//         _height = height; 
+//     }
+//
+//     public int getArea()
+//     {
+//         return _width * _height;
+//     }
+// }
+//
+// public class Square : Rectangle
+// {
+//     public override void setWidth(int width)
+//     {
+//         base.setWidth(width);
+//         base.setHeight(width);
+//     }
+//
+//     public override void setHeight(int height)
+//     {
+//         base.setHeight(height);
+//         base.setWidth(height);
+//     }
+// }
