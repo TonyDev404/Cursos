c78fd4a7a5ea66598f0b8f0e195d87d2bda6852a
Antonio Narvaez <jnarvaez@logsys.com.mx>
2025-07-28 16:36:21 -0600

    Se continua con proyecto React Quiz

---
+++ React/ultimate-react-course/07-tony-react-quiz/src/components/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Principal from './Principal';
+import Loader from './Loader';
+import Error from './Error';
+import Question from './Question';
+import NextButton from './NextButton';
+import StartScreen from './StartScreen';
+
+const initialState= {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'activate', 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { 
+        ...state, 
+        questions: action.payload, 
+        status: "ready"
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: "error"
+      }
+    case 'start':
+      return {
+        ...state,
+        status: 'active'  
+      }
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points: action.payload === question.correctOption 
+        ? state.points + question.points 
+        : state.points,
+      }
+    case 'nextQuestion':
+      return {
+        ...state, 
+        index: state.index + 1,
+        answer: null,
+      }
+    default:
+      throw new Error('Action unkown');
+
+  }
+};
+
+export default function App() {
+
+  const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function() {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data}))
+      .catch((err) => dispatch({type: 'dataFailed'}));
+  }, [])
+
+  return (
+    <div className='app'>
+      <Header />
+
+      <Principal>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (<StartScreen numQuestions={numQuestions} dispatch={dispatch} />)}
+        {status === 'active' && (
+          <>
+            <Question 
+              question={questions[index]} 
+              dispatch={dispatch} 
+              answer={answer}
+            />
+              <NextButton 
+                dispatch={dispatch} 
+                answer={answer}
+              />
+          </> 
+        )}
+      </Principal>
+    </div>
+  )
+}
+
+    // "server": "json-server --watch data/questions.json --port 8000" // "json-server" is used to mock a backend server
